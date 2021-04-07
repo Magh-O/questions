1899a4bc4353243723c64748a55557d34df6757a
Adrián Moure Cerdeira <amc.3da@gmail.com>
2021-04-07 17:14:52 +0200

    Added the TODOs from the main program and the gitignore file

---
+++ questions.py
@@ -0,0 +1,16 @@
+#This simple program gets a json file to generate the questions, shows it to the user and asks her 
+#for an answer, if the user checks it and thinks its correct annotates one point to the score 
+
+#TODO - Read a json file
+
+#TODO - Create the questions
+
+#TODO - Create the user and the score
+
+#TODO - Loop to answer the questions to the user and get the user guess
+
+#TODO - Show comparison between guessed and correct answer
+
+#TODO - Ask the user if it was correct and update the score
+
+#TODO - At the end print the score
