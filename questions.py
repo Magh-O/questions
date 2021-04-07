3f1c5232d07ac194a39301238de1f956fa7b5733
Adrián Moure Cerdeira <amc.3da@gmail.com>
2021-04-07 17:53:56 +0200

    Added the create list function

--- questions.py
+++ questions.py
@@ -1,9 +1,25 @@
 #This simple program gets a json file to generate the questions, shows it to the user and asks her 
 #for an answer, if the user checks it and thinks its correct annotates one point to the score 
 
-#TODO - Read a json file
+import json
+JSON_PATH = "to-study.json"
 
-#TODO - Create the questions
+#TODO - Read a json file
+def read_json(filepath):
+
+    with open(filepath, "r", encoding="utf-8") as q_file:
+        questions = json.load(q_file)
+
+    return questions
+
+#TODO - Create a list of questions
+def create_question_list(q_dict):
+
+    question_list = []
+    for question, answer in read_json(q_dict).items():
+        question_list.append((question, answer))
+
+    return question_list
 
 #TODO - Create the user and the score
 
